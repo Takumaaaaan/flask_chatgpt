7b8e67905dde7246d82ff3d8d00a2771a5584b0c
Takumaaaaan <taakunn2008@icloud.com>
2023-03-13 11:06:53 +0900

    add deepl

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, jsonify, request, session, redirect
 from datetime import timedelta
 import openai
+import deepl
 from dotenv import load_dotenv
-
-load_dotenv()
-# 環境変数を参照
 import os
-API_KEY = os.getenv('API_KEY')
+load_dotenv()
 
+#Flaskの設定
 app = Flask(__name__, static_folder='./static')
+"""
+環境変数の設定
+"""
+API_KEY = os.getenv('API_KEY')
+if os.getenv("HTTP_PROXY"):
+    os.environ['http_proxy'] = os.getenv("HTTP_PROXY")
+if os.getenv("HTTP_PROXY"):    
+    os.environ['https_proxy'] = os.getenv("HTTPS_PROXY")
 
+#flaskのセッションキー
 app.secret_key = os.getenv('SECRET_KEY')
 app.permanent_session_lifetime = timedelta(minutes=30)
+#deeplのAPIキー
+DEEPL_API_KEY = os.environ["DEEPL_API_KEY"]
 
+"""
+チャットの設定
+"""
 def completion(new_message_text:str, settings_text:str = '', past_messages:list = []):
     openai.api_key = API_KEY
     print(settings_text)
@@ -32,8 +45,62 @@ def completion(new_message_text:str, settings_text:str = '', past_messages:list 
     response_message_text = result.choices[0].message.content
     
     return response_message_text, past_messages
+"""
+翻訳の設定
+"""
+def translate_text(text:str,target_lang:str):
+    if os.getenv("HTTP_PROXY"):
+        translator = deepl.Translator(DEEPL_API_KEY,proxy=os.environ['https_proxy'])
+    else:
+        translator = deepl.Translator(DEEPL_API_KEY)
+    print(text)
+    print(target_lang)
+    result = translator.translate_text(text=text,target_lang=target_lang)
+    
+    return result.text
+
+"""
+翻訳のルータ
+"""
+
+@app.route('/reload_translate/', methods=['GET'])
+def reload_translate():
+    if "all_translates" in session:
+        session["all_translates"] = []
+    if "past_messages" in session:
+        session["past_translates"] = []
+    return redirect("/translate/")
+
+@app.route('/translate/', methods=['GET','POST'])
+def tranlate():
+    if "all_translates" not in session:
+        session["all_translates"] = []
+
+    if request.method == "GET":
+        return render_template('translate.html',mode="翻訳モード")
+    
+    elif request.method == "POST":
+        
+        all_translates = session["all_translates"]
+        message = request.form['message']
+        if message is None:
+            return render_template('translate.html',mode="翻訳モード")
+
+        english_flag = message.encode('utf-8').isalpha()
+        if english_flag:
+            response = translate_text(text=message,target_lang="JA")
+        else:
+            response = translate_text(text=message,target_lang="EN-US")
+        all_translates.append(message)
+        all_translates.append(response)
+        session["all_translates"] = all_translates
+
+        return render_template('translate.html', messages=all_translates,mode="翻訳モード")
 
 
+"""
+チャットのルーターの設定
+"""
 @app.route('/reload/', methods=['GET'])
 def reload():
     if "all_messages" in session:
@@ -136,9 +203,9 @@ def chat_english():
 
 @app.route('/japanese/', methods=['GET',"POST"])
 def chat_japanese():
-    setting = "あなたは優秀なプロの編集者です。私が送る日本語の誤字、脱字、タイプミス、文法ミス、不自然な表現を全て指摘し、"\
-        "適切な表現に全て訂正してください。全ての訂正箇所を小学生にもわかるように一つずつ箇条書きで説明してください。"\
-        "また、訂正後の文章を全文表示してください。"
+    setting = "あなたはプロの編集者です。私が送る文章全ての誤字、脱字、タイプミスを全て指摘してください。"\
+        "全ての指摘した内容を箇条書きで記載し説明してください。"\
+        "全ての指摘した内容を正しい日本語に修正し、修正後の文章を全文表示してください。"
     if "all_messages" not in session:
         session["all_messages"] = []
     if "past_messages" not in session:
